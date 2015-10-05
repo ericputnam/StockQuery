99640da13c3857ff365f565db96938b90c9160a4
ericputnam <egp9460@gmail.com>
2015-10-05 15:25:10 -0400

    Updated the load process to load each month of a certain time period.

--- import/import.js
+++ import/import.js
@@ -8,6 +8,8 @@ var database = _interopRequireWildcard(require("./database"));
 
 var objectToInsert = {}; //final object to be inserted
 
+var moment = require("moment"); //JS plugin called moment, that helps format dates. We use this in order to help with query ranges.
+
 /*
 processImport function returns a promise containing all the jpg and/or jpeg
 files from a given folder that is provided as a paramter (2nd argument for the
@@ -16,24 +18,38 @@ script)
 var processImport = function () {
 
   var promise = new Promise(function (resolve, reject) {
-    //var result = [];
+    var startDate = moment("2015-10-01");
+    var endDate = moment("2015-01-01");
 
-    yahoofinancelookup.makeRequest().then(function (result) {
+    while(startDate > endDate){
+        var queryEndDate = startDate.format("YYYY-MM-DD");
+        var queryStartDate = startDate.subtract(1, 'months').format("YYYY-MM-DD")
+         
+        console.log("Query Range-> " + queryEndDate + " to " + queryStartDate);
+        //var dateString = start.format("yyy-mm-dd");
 
-      objectToInsert.filename = result.filename;
-      objectToInsert.jsondata = result.jsondata;
-      console.log(objectToInsert.filename);
+        yahoofinancelookup.makeRequest(queryStartDate, queryEndDate).then(function (result) {
 
-      database.insert("JSON", "", objectToInsert).then(function (response) {
-              console.log("JSON file inserted ", response.documents[0].uri);
+        objectToInsert.filename = result.filename;
+        objectToInsert.jsondata = result.jsondata;
+        console.log(objectToInsert.filename);
+
+        database.insert("JSON", "", objectToInsert).then(function (response) {
+              console.log("JSON file inserted for month -->", response.documents[0].uri);
             })["catch"](function (error) {
               console.log(error);
             });
+      })["catch"](function (error) {
+          console.log(error);
+      });
 
-  return promise;
-  })["catch"](function (error) {
+      //console.log(dateString);
+    }
+
+    return promise;
+
+})["catch"](function (error) {
       console.log(error);
-  });
 });
   
 };
--- import/yahoofinancelookup.js
+++ import/yahoofinancelookup.js
@@ -9,18 +9,22 @@ Model to go to the noaa website and pull down information
 */
 
 var http = require("http");
-var jsesc = require('jsesc');
+var moment = require("moment");
 //require("es6-promise").polyfill();
 
-var makeRequest = function () {
+var makeRequest = function (startDate, endDate) {
   var promise = new Promise(function (resolve, reject) {
     var result = {};
-    var startDate = "2015-10-01";
-    var endDate = "2015-10-02";
-      
+    //var startDate = "2015-10-01";
+    //var endDate = "2015-10-02";
+    console.log("START" + moment(startDate).format("YYYY-MM-DD"));
+    console.log("END" + moment(endDate).format("YYYY-MM-DD"));
+    console.log("http://query.yahooapis.com" + "/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20in%20(%22AA%22%2C%22AXP%22%2C%22BA%22%2C%22BAC%22%2C%22CAT%22%2C%22CSCO%22%2C%22CVX%22%2C%22DD%22%2C%22DIS%22%2C%22GE%22%2C%22HD%22%2C%22HPQ%22%2C%22IBM%22%2C%22INTC%22%2C%22JNJ%22%2C%22JPM%22%2C%22KFT%22%2C%22KO%22%2C%22MCD%22%2C%22MMM%22%2C%22MRK%22%2C%22MSFT%22%2C%22PFE%22%2C%22PG%22%2C%22T%22%2C%22TRV%22%2C%22UTX%22%2C%22VZ%22%2C%22WMT%22%2C%22XOM%22)%20and%20startDate%20%3D%20%22" + moment(startDate).format("YYYY-MM-DD") + "%22%20and%20endDate%20%3D%20%22" + moment(endDate).format("YYYY-MM-DD") + "%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
+    
+
       var options = {
         hostname: "query.yahooapis.com",
-        path: "/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20in%20(%22AA%22%2C%22AXP%22%2C%22BA%22%2C%22BAC%22%2C%22CAT%22%2C%22CSCO%22%2C%22CVX%22%2C%22DD%22%2C%22DIS%22%2C%22GE%22%2C%22HD%22%2C%22HPQ%22%2C%22IBM%22%2C%22INTC%22%2C%22JNJ%22%2C%22JPM%22%2C%22KFT%22%2C%22KO%22%2C%22MCD%22%2C%22MMM%22%2C%22MRK%22%2C%22MSFT%22%2C%22PFE%22%2C%22PG%22%2C%22T%22%2C%22TRV%22%2C%22UTX%22%2C%22VZ%22%2C%22WMT%22%2C%22XOM%22)%20and%20startDate%20%3D%20%22" + startDate + "%22%20and%20endDate%20%3D%20%22" + endDate + "%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
+        path: "/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20in%20(%22AA%22%2C%22AXP%22%2C%22BA%22%2C%22BAC%22%2C%22CAT%22%2C%22CSCO%22%2C%22CVX%22%2C%22DD%22%2C%22DIS%22%2C%22GE%22%2C%22HD%22%2C%22HPQ%22%2C%22IBM%22%2C%22INTC%22%2C%22JNJ%22%2C%22JPM%22%2C%22KFT%22%2C%22KO%22%2C%22MCD%22%2C%22MMM%22%2C%22MRK%22%2C%22MSFT%22%2C%22PFE%22%2C%22PG%22%2C%22T%22%2C%22TRV%22%2C%22UTX%22%2C%22VZ%22%2C%22WMT%22%2C%22XOM%22)%20and%20startDate%20%3D%20%22" + moment(startDate).format("YYYY-MM-DD") + "%22%20and%20endDate%20%3D%20%22" + moment(endDate).format("YYYY-MM-DD") + "%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
         method: "GET"
       };
       
@@ -34,7 +38,7 @@ var makeRequest = function () {
 
           response.on('end', function(){
               //callback(data);
-              console.log(data);
+              console.log("Received data back. Waaahooo!");
               result = {
                 jsondata: data,
                 filename : startDate
